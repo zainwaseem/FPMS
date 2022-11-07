d61036adf5e536ece161b0f54bf25466bca86f55
zainwaseem <zainwaseem9371@gmail.com>
2022-11-07 23:38:08 +0500

    model

--- server/app.js
+++ server/app.js
@@ -1,8 +1,9 @@
-console.log(`zain`);
-console.log(`zain`);
-console.log(`zain`);
-console.log(`zain`);
-console.log(`zain`);
-console.log(`zain`);
-console.log(`zain`);
-console.log(`just xhexking`);
+import express from "express";
+import myroute from "./routes/adminRoutes.js";
+
+const app = express();
+
+app.use("/", myroute);
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
